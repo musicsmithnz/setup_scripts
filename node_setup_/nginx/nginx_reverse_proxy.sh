b1595b3f92f1778e33441a81881efc345195f71d
raymond <raymond@lucentmonkey.com>
2017-11-28 19:39:17 +0000

    nginx

--- node_setup_/nginx/nginx_reverse_proxy.sh
+++ node_setup_/nginx/nginx_reverse_proxy.sh
@@ -3,15 +3,16 @@
 app_name=lucentmonkey.com
 web_dir=/var/www
 script_dir=$(pwd)
+script_sub_dir=node_setup/nginx
 
 yum -y update
 yum -y install epel-release
 yum -y install nginx
 
 mkdir /etc/nginx/sites-available /etc/nginx/sites-enabled
-cp ${script_dir}/nginx.conf /etc/nginx/nginx.conf
-cp ${script_dir}/app_name.conf /etc/nginx/sites-available/${app_name}.conf
-ln -s /etc/nginx/sites-available/${app_name}/etc/nginx/sites-enabled/${app_name}
+cp ${script_dir}/${script_sub_dir}/nginx.conf /etc/nginx/nginx.conf
+cp ${script_dir}/${script_sub_dir}/app_name.conf /etc/nginx/sites-available/${app_name}.conf
+ln -s /etc/nginx/sites-available/${app_name} /etc/nginx/sites-enabled/${app_name}
 sed -i "s/APP_NAME/${app_name}/" /etc/nginx/sites-available/${app_name}
 sed -i "s/WEB_DIR/${web_dir}/" /etc/nginx/sites-available/${app_name}
 
